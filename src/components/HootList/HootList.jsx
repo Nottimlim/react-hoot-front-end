632e39e1fb575c90f97b9d43a1dc56bf08714ea3
Mandy2114 <mandydecamp1213@gmail.com>
2024-10-24 10:34:44 -0400

    HootList test2

--- src/components/HootList/HootList.jsx
+++ src/components/HootList/HootList.jsx
@@ -1,23 +1,28 @@
 import { Link } from 'react-router-dom';
 
-const HootList = (props) => {
+
+const HootList = ({ hoots }) => {
+  if (!Array.isArray(hoots) || hoots.length === 0) {
+    return <p>No hoots available.</p>;
+  }
+
   return (
     <main>
-    {props.hoots.map((hoot) => (
-      <Link key={hoot._id} to={`/hoots/${hoot._id}`}>
-        <article>
-          <header>
-            <h2>{hoot.title}</h2>
-            <p>
-              {hoot.author.username} posted on {' '}
-              {new Date(hoot.createdAt).toLocaleDateString()}
-            </p>
-          </header>
-          <p>{hoot.text}</p>
-        </article>
-      </Link>
-    ))}
-  </main>
+      {hoots.map((hoot) => (
+        <Link key={hoot._id} to={`/hoots/${hoot._id}`}>
+          <article>
+            <header>
+              <h2>{hoot.title}</h2>
+              <p>
+                {hoot.author.username} posted on{' '}
+                {new Date(hoot.createdAt).toLocaleDateString()}
+              </p>
+            </header>
+            <p>{hoot.text}</p>
+          </article>
+        </Link>
+      ))}
+    </main>
   );
 };
 
