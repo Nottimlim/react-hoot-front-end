a63ef4b5fa0ee4e4162a66cb2ce4794183a851a3
Timothy Lim <contacttimothylim@gmail.com>
2024-10-24 09:54:14 -0400

    update hoot, create comment

--- src/services/hootService.js
+++ src/services/hootService.js
@@ -45,19 +45,19 @@ const create = async (hootFormData) => {
 // update hoot (PUT method /hoots/:hootId)
 const update = async (hootId, hootFormData) => {
   try {
-    const res = await fetch(`${BASE_URL}/hoots/${hootId}`, {
+    const res = await fetch(`${BASE_URL}/${hootId}`, {
       method: 'PUT',
       headers: {
         Authorization: `Bearer ${localStorage.getItem('token')}`,
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(hootFormData)
+      body: JSON.stringify(hootFormData),
     });
     return res.json();
   } catch (error) {
     console.log(error);
   }
-};
+}
 
 // delete hoot (DELETE method /hoots/:hootId)
 const deleteHoot = async (hootId) => {
