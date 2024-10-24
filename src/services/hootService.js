9660b5dd8327c4dc734efa32f1e2106b650ec893
Mandy2114 <mandydecamp1213@gmail.com>
2024-10-24 09:27:31 -0400

    Delete function

--- src/services/hootService.js
+++ src/services/hootService.js
@@ -1,9 +1,36 @@
-// src/services/hootService.js
+// const BASE_URL = `${import.meta.env.VITE_EXPRESS_BACKEND_URL}/hoots`;
+
+const index = async () => {
+  try {
+    const res = await fetch(BASE_URL, {
+      headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+    });
+    return res.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const show = async (hootId) => {
   try {
     const res = await fetch(`${BASE_URL}/${hootId}`, {
-      headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+      headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+    });
+    return res.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const create = async (hootFormData) => {
+  try {
+    const res = await fetch(BASE_URL, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(hootFormData),
     });
     return res.json();
   } catch (error) {
@@ -14,10 +41,10 @@ const show = async (hootId) => {
 const createComment = async (hootId, commentFormData) => {
   try {
     const res = await fetch(`${BASE_URL}/${hootId}/comments`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        Authorization: `Bearer ${localStorage.getItem('token')}`,
-        'Content-Type': 'application/json',
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(commentFormData),
     });
@@ -30,9 +57,9 @@ const createComment = async (hootId, commentFormData) => {
 const deleteHoot = async (hootId) => {
   try {
     const res = await fetch(`${BASE_URL}/${hootId}`, {
-      method: 'DELETE',
+      method: "DELETE",
       headers: {
-        Authorization: `Bearer ${localStorage.getItem('token')}`,
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
       },
     });
     return res.json();
@@ -41,10 +68,4 @@ const deleteHoot = async (hootId) => {
   }
 };
 
-export {
-  index,
-  show,
-  create,
-  createComment,
-  deleteHoot,
-};
+export { index, show, create, createComment, deleteHoot };
